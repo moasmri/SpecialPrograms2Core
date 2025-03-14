a4f40798e51a33e8e51f05dcdc671aeda42cf5e0
moasmri <mo.asmri@gmail.com>
2025-03-15 02:58:42 +0300

    Add project files.

---
+++ SpecialPrograms2Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpecialPrograms2Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
