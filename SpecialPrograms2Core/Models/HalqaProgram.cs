436367d43334bba04b745e0f10357da7f3fb10a9
moasmri <mo.asmri@gmail.com>
2025-03-15 05:11:21 +0300

    Scaffolding

--- SpecialPrograms2Core/Models/HalqaProgram.cs
+++ SpecialPrograms2Core/Models/HalqaProgram.cs
@@ -1,4 +1,7 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SpecialPrograms2Core.Data;
 
 namespace SpecialPrograms2Core.Models
 {
